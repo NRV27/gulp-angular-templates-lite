ec3895011ee47b236bf4a23de40d0ff6e59e96c2
Aaron Schinkowitch <aaron.schinkowitch@gmail.com>
2021-10-25 11:56:26 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const Transform = require('stream').Transform;
+const PluginError = require('plugin-error');
+
+const PLUGIN_NAME = 'gulp-angular-templates-lite';
+
+module.exports = function () {
+    const transformStream = new Transform({objectMode: true});
+
+    /**
+     * @param {Buffer|string} file
+     * @param {string=} encoding - ignored if file contains a Buffer
+     * @param {function(Error, object)} callback - Call this function (optionally with an
+     *          error argument and data) when you are done processing the supplied chunk.
+     */
+    transformStream._transform = function(file, encoding, callback) {
+        if (!file) {
+            // nothing to do
+            return callback(null, file);
+        }
+
+        const path = file.history[0];
+        if (!path.startsWith(file._base)) {
+            throw new PluginError(PLUGIN_NAME, new Error('Unable to calculate relative path'));
+        }
+        const relativePath = path.substr(file._base.length);
+
+        file._contents = Buffer.from(getJs(relativePath, file._contents.toString(encoding)));
+
+        callback(null, file, encoding);
+    };
+
+    function getJs (relativePath, html) {
+        return '\nm.run([\'$templateCache\',function(t){t.put(\'' + relativePath + '\',\'' + toJsString(html)
+            + '\')}]);';
+    }
+
+    function toJsString (html) {
+        return html.replace(/\\/g, '\\\\')
+            .replace(/'/g, '\\\'')
+            .replace(/\r?\n/g, "\\n");
+    }
+
+    return transformStream;
+};
---
+++ test/test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+require('mocha');
+
+const toJs = require('../index');
+const Vinyl = require('vinyl');
+const assert = require('assert');
+
+describe('transforms html to js', () => {
+    it ('transforms the html files', done => {
+        const stream = toJs();
+
+        stream.on('data', function (file) {
+            assert.strictEqual(file.relative, 'template-a.html');
+            assert.strictEqual(file.contents.toString('utf8'), '\nm.run([\'$templateCache\',function(t){t.put(\'/template-a.html\',\'<h1 id="template-a">I\\\'m template A!</h1>\')}]);');
+            done();
+        });
+
+        stream.write(new Vinyl({
+            base: __dirname,
+            path: __dirname + '/template-a.html',
+            contents: Buffer.from('<h1 id="template-a">I\'m template A!</h1>')
+        }));
+        stream.end();
+    });
+});
